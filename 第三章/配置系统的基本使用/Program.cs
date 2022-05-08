a3b0f59e1b54ae46a108674f0c35db7cc1e84281
xwj-june <35751931+xwj-june@users.noreply.github.com>
2022-05-08 22:43:41 +1200

    binding config data to class

--- 第三章/配置系统的基本使用/Program.cs
+++ 第三章/配置系统的基本使用/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿//.NET6教程 Part2-32：配置系统1 入门
+//https://youtu.be/pv8J0zBKCKg
+
+using Microsoft.Extensions.Configuration;
 
 //手动读取配置
 ConfigurationBuilder configBuilder = new ConfigurationBuilder();
@@ -8,3 +11,21 @@ string name = config["name"];
 Console.WriteLine($"name={name}");
 string proxyAddress = config.GetSection("proxy:address").Value;
 Console.WriteLine($"Address:{proxyAddress}");
+
+//Binding configuration data into class
+//NuGet package: Microsoft.Extensions.Configuration.Binder
+var proxy = config.GetSection("proxy").Get<Proxy>();
+Console.WriteLine(proxy.Address);
+Config config = configRoot.Get<Config>();
+Console.WriteLine(config.Name);
+
+class Config{
+    public string Name {get; set;}
+    public int Age {get; set;}
+    public Proxy Proxy {get; set;}
+}
+
+class Proxy{
+    public string Address {get; set;}
+    public string Port {get; set;}
+}
