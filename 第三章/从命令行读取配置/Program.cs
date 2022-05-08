72dd2bf26284c01869f0c7bc73c014428f76bfaf
xwj-june <35751931+xwj-june@users.noreply.github.com>
2022-05-08 23:13:54 +1200

    process passed in config data from command line and env variable

--- 第三章/从命令行读取配置/Program.cs
+++ 第三章/从命令行读取配置/Program.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿//.NET Core教程 Part2-34：配置系统3 其他配置提供者
+//https://youtu.be/GimWVOySY7o
+using Microsoft.Extensions.Configuration;
 
 ConfigurationBuilder configBuilder = new ConfigurationBuilder();
 configBuilder.AddCommandLine(args);
 IConfigurationRoot config = configBuilder.Build();
 string server = config["server"];
 Console.WriteLine($"server:{server}");
+
+//when passing complex data in command lines
+//e.g. object in Json
+//class "Proxy" - proprty array "Ids"
+//To set set first id to be 5 in command lines: 
+//Proxy:Ids:1 = 5
--- 第三章/从环境变量读取配置/Program.cs
+++ 第三章/从环境变量读取配置/Program.cs
@@ -5,3 +5,9 @@ configBuilder.AddEnvironmentVariables("TEST_");
 IConfigurationRoot configRoot = configBuilder.Build();
 string name = configRoot["Name"];
 Console.WriteLine(name);
+
+//when passing complex data in environment variable 
+//e.g. object in Json
+//class "Proxy" - proprty array "Ids"
+//To set set first id to be 5 in command lines: 
+//Proxy:Ids:1 = 5
